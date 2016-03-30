37fd98ae4a3c4c0b02e6d3debf608d25cd4d01d7
Giselle Tavares <gitavares@hotmail.com>
2016-03-29 21:05:13 -0300

    Subindo as versoes iniciais da aplicação

---
+++ src/br/com/tiqe/bean/UserBean.java
@@ -0,0 +1,27 @@
+package br.com.tiqe.bean;
+
+//import javax.faces.bean.ManagedBean;
+import javax.annotation.ManagedBean;
+
+import br.com.tiqe.modelo.User;
+
+@ManagedBean
+public class UserBean {
+
+	private User user = new User();
+	
+	public User getUser(){
+		return user;
+	}
+	
+	public void setUser(){
+		System.out.println("Gravando usuario... " + this.user.getFirstName() + " " + this.user.getLastName());
+		
+		if(user.getFirstName().isEmpty()){
+			throw new RuntimeException("Usuario deve ter um nome");
+		}
+		
+		new DAO<User>(User.class).insert(this.user);
+		
+	}
+}
